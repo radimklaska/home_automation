05ae6e78b4a3b6fb35194d21c4b768a677311ae5
Radim Klaska <radim@klaska.net>
2014-05-19 16:55:12 +0200

    Test temperature and humidity readings

---
+++ arduino_home.ino
@@ -0,0 +1,49 @@
+#include <DHT11.h>
+int pin=2;
+DHT11 dht11(pin);
+
+// delta max = 0.6544 wrt dewPoint()
+// 6.9 x faster than dewPoint()
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double dewPointFast(double celsius, double humidity)
+{
+  double a = 17.271;
+  double b = 237.7;
+  double temp = (a * celsius) / (b + celsius) + log(humidity*0.01);
+  double Td = (b * temp) / (a - temp);
+  return Td;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+}
+
+void loop()
+{
+  int err;
+  float temp, humi;
+  if((err=dht11.read(humi, temp))==0)
+  {
+    Serial.print("temperature: ");
+    Serial.print(temp);
+    Serial.print(" humidity: ");
+    Serial.print(humi);
+    Serial.print(" dew point: ");
+    Serial.print(dewPointFast(temp, humi));
+    Serial.println();
+  }
+  else
+  {
+    Serial.println();
+    Serial.print("Error No :");
+    Serial.print(err);
+    Serial.println();    
+  }
+  delay(5000); //delay for reread
+}
+
+
